5144a7d672c0211ace9e9571ccc22d075a222971
Joost Valentijn <joost.valentijn@hva.nl>
2022-01-29 12:21:50 +0100

    changed direction of normal where topright has two positive numbers

--- sources/systems/headingEnemySystem.js
+++ sources/systems/headingEnemySystem.js
@@ -1,8 +1,8 @@
 export default (scene) => {
   scene.query(['position', 'target', 'heading'], ({ position, target, heading }) => {
     const headingTemp = [
-      position[0] - target[0],
-      position[1] - target[1],
+      target[0] - position[0],
+      target[1] - position[1],
     ];
     const [x, y] = headingTemp;
     const length = Math.sqrt(x * x + y * y); // calculating length of vector for normal
--- sources/systems/headingPlayerSystem.js
+++ sources/systems/headingPlayerSystem.js
@@ -5,12 +5,14 @@ export default (scene) => {
   });
   scene.query(['position', 'heading'], ({ position, heading }) => {
     const headingTemp = [
-      position[0] - mousePos[0],
-      position[1] - mousePos[1],
+      mousePos[0] - position[0],
+      mousePos[1] - position[1],
     ];
     const [x, y] = headingTemp;
     const length = Math.sqrt(x * x + y * y); // calculating length of vector for normal
     heading[0] = headingTemp[0] / length; // set heading
     heading[1] = headingTemp[1] / length;
+
+    console.log(heading);
   }, ['active', 'player']);
 };
