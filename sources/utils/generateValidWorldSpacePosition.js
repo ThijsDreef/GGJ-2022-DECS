97a4056682148dbbf682484db9994dd3996cde26
Noorts <50550612+Noorts@users.noreply.github.com>
2022-01-30 13:49:51 +0100

    Add safe spawn system

--- sources/utils/generateValidWorldSpacePosition.js
+++ sources/utils/generateValidWorldSpacePosition.js
@@ -1,7 +1,22 @@
 import { BOUNDS, X_DIFF, Y_DIFF } from '../WorkSpaceBounds';
+import mapFromTile from './mapFromTile';
 
-export default () => {
+export function generateValidWorldSpacePosition() {
   const randomValidX = BOUNDS[0][0] + Math.random() * X_DIFF;
   const randomValidY = BOUNDS[0][1] + Math.random() * Y_DIFF;
   return [randomValidX, randomValidY];
-};
+}
+
+const SAFE_DISTANCE = 10;
+export function generateValidSafeWorldSpacePosition(rawPlayerPosition) {
+  const playerPosition = mapFromTile(rawPlayerPosition[0], rawPlayerPosition[1], rawPlayerPosition[2]);
+
+  while (true) {
+    const validWorldPosition = generateValidWorldSpacePosition();
+    const distanceX = validWorldPosition[0] - playerPosition[0];
+    const distanceY = validWorldPosition[1] - playerPosition[1];
+    const distance = Math.sqrt((distanceX ** 2) + (distanceY ** 2));
+
+    if (distance > SAFE_DISTANCE) return validWorldPosition;
+  }
+}
---
+++ sources/utils/mapFromTile.js
@@ -0,0 +1,1 @@
+export default (x, y, z) => [x / 32, y / 32, z];
